6a1d05d2a10c1a59fcfc8f0fcd4e6dfef91563d2
s3rj1k <evasive.gyron@gmail.com>
2019-11-07 18:41:18 +0200

    remove silenced default logger code, upstream fix
    Signed-off-by: s3rj1k <evasive.gyron@gmail.com>

--- signalwire/jrpc2.go
+++ signalwire/jrpc2.go
@@ -1,10 +1,5 @@
 package signalwire
 
-import (
-	"io/ioutil"
-	"log"
-)
-
 // Jsonrpc2Logger is a wrapper for sourcegraph jsonrpc2 logger
 type Jsonrpc2Logger struct{}
 
@@ -16,14 +11,3 @@ func (l *Jsonrpc2Logger) Printf(format string, args ...interface{}) {
 }
 
 //revive:enable:unused-receiver
-
-/*
-  We need to silence default logger, for some strange reason
-  sourcegraph/jsonrpc2 uses Log to print debug message without
-  any way of disabling this behavior.
-  https://github.com/sourcegraph/jsonrpc2/blob/master/jsonrpc2.go#L579
-*/
-
-func init() { // nolint: gochecknoinits
-	log.SetOutput(ioutil.Discard)
-}
